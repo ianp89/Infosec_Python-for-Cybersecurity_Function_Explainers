2c65c8bbed7ff00738bc086ce632637cc96eefc2
Ian Pokres <80968123+ianp89@users.noreply.github.com>
2024-07-19 15:27:12 -0400

    Commented on NonApplicationServer.py to explain functionality.

--- Part_13/13.2_Non-Application_Layer_Protocol/NonApplicationServer.py
+++ Part_13/13.2_Non-Application_Layer_Protocol/NonApplicationServer.py
@@ -1,7 +1,11 @@
+# This script uses scapy to sniff for ICMP packets, isolated exfiltrated data within those packets, and print them to the console.
+
 from scapy.all import *
 
 def printData(x):
     d = chr(x[ICMP].code)
     print(d,end="",flush=True)
+# Isolates desired data hidden in ICMP packets, and prints.
 
 sniff(filter="icmp", prn=printData)
+# Prints sniffed ICMP packet data.
