089c809a037a94e90ef48f27ded2db96f91c2e09
Ian Pokres <80968123+ianp89@users.noreply.github.com>
2024-07-19 13:56:17 -0400

    Commented on EncryptedChannelClient.py to explain functionality.

--- Part_12/12.1_Encrypted_Channel/EncryptedChannelClient.py
+++ Part_12/12.1_Encrypted_Channel/EncryptedChannelClient.py
@@ -1,3 +1,5 @@
+# This script sets up an AES client to encrypt messages sent on a specific port.
+
 import socket, os
 from Crypto.Cipher import AES
 
@@ -10,7 +12,7 @@ key = b"Sixteen byte key"
 
 def encrypt(data,key,iv):
     data += " "*(16 - len(data) % 16)
-    # AES only works for data in 16 byte blocks, so this line finds how many spaces necessary to complete 16 bytes.
+    # AES only works for data in 16 byte blocks, so this line finds how many spaces necessary to complete 16 bytes (= 'padding').
     cipher = AES.new(key,AES.MODE_CBC,iv)
     # CBC is a "mode of operation," needs to be identical on both sides of the connection;
     #basically establishes how the encryption method deals with data longer than its maximum encryptable datablock size.
@@ -20,9 +22,14 @@ message = "Hello"
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((host,port))
+    # Python's socket library is used here to send data through the specificied port (here = 1337).
     iv = os.urandom(16)
     s.send(iv)
+    # Appropriately sized Initialization Vector (iv) is used to initiate AES cipher in CBC mode. IV's are used to further obscure data by 
+    # adding an element of randomness to the beginning of a data stream.
     s.send(bytes([len(message)]))
+    # Informs the server how long the message is (could be implemented in padding statement).
     encrypted = encrypt(message,key,iv)
     print("Sending %s" % encrypted.hex())
     s.sendall(encrypted)
+    # Encrypted message sent.
