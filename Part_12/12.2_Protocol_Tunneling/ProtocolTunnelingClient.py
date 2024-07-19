c1d7103688eb5c10f809c13801d320cf88bcec42
Ian Pokres <80968123+ianp89@users.noreply.github.com>
2024-07-19 14:14:37 -0400

    Commented on ProtocolTunnelingClient.py to explain functionality.

--- Part_12/12.2_Protocol_Tunneling/ProtocolTunnelingClient.py
+++ Part_12/12.2_Protocol_Tunneling/ProtocolTunnelingClient.py
@@ -1,4 +1,9 @@
+# This script emplaces an encoded piece of data into an HTTP cookie, which might help evade detection,
+# because data is often obscured with encoding in that area of an HTTP transaction.
+# To make the tunneling more effective, the request would have to mimic an actual HTTP transaction more closely, and the URL would have to change.
+
 import requests
+# Python's requests library is its HTTP client and handler.
 from base64 import b64encode,b64decode
 
 def C2(url,data):
