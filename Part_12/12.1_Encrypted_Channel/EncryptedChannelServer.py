29fe6894e4f292050ac258663a7e63db24c908bf
Ian Pokres <80968123+ianp89@users.noreply.github.com>
2024-07-19 14:02:58 -0400

    Commented on EncryptedChannelServer.py to explain functionality.

--- Part_12/12.1_Encrypted_Channel/EncryptedChannelServer.py
+++ Part_12/12.1_Encrypted_Channel/EncryptedChannelServer.py
@@ -1,3 +1,5 @@
+# This script sets up the AES server/recipient, connected to mirrored "client."
+
 import socket
 from Crypto.Cipher import AES
 
@@ -8,17 +10,22 @@ key = b"Sixteen byte key"
 def decrypt(data,key,iv):
     cipher = AES.new(key,AES.MODE_CBC,iv)
     return cipher.decrypt(data)
-# See client-side script for explanation up to this point.
 
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+# See client-side script for explanation up to this point.
     s.bind((host,port))
     s.listen()
+    # "Listens" on the port specified in the bind() function, rather than "connecting" on the client side.
     conn,addr = s.accept()
     with conn:
         while True:
+        # An Infinite loop with the client is established if the "accepted" connection is established.
             iv = conn.recv(16)
-            length = conn.recv(1)   # Assumes short messages
+            length = conn.recv(1)   
+            # It assumes only a single byte message - in this case - after the IV.
             data = conn.recv(1024)
+            # the recv() function sets a max data limit. In this case it has a lot of padding, but will not result in an error in such a case.
             if not data:
                 break
             print("Received: %s"%decrypt(data,key,iv).decode("utf-8")[:ord(length)])
+            # Does the actual decrypting, and prints the results without "padding."
