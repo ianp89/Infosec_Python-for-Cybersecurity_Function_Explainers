8642ea7f62f2b8db2ab11f0a43ea0d616b654014
Ian Pokres <80968123+ianp89@users.noreply.github.com>
2024-07-19 17:18:37 -0400

    Commented on DecoyContent.py to explain functionality.

--- Part_15/15.2_Decoy_Content/DecoyContent.py
+++ Part_15/15.2_Decoy_Content/DecoyContent.py
@@ -1,31 +1,39 @@
+# This script catches threat actors meddling with decoy files (which would be files set up to look enticing to catch just such people).
+
 import pathlib
 
 def getTimestamps(filename):
     fname = pathlib.Path(filename)
     stats = fname.stat()
-    if not fname.exists(): # File deleted
+    if not fname.exists(): 
         return []
+        # This would mean the file got deleted...also a clear sign of tampering.
     return(stats.st_ctime,stats.st_mtime,stats.st_atime)
+    # The format of decoys.txt is such that it records legitimate creation (c), modification (m), and access (a) times.
     
 
 def checkTimestamps(filename,create,modify,access):
     stats = getTimestamps(filename)
     if len(stats) == 0:
-        return False # File deleted
+        return False 
     (ctime,mtime,atime) = stats
     if float(create) != float(ctime):
-        return False    # File creation time is incorrect
+        return False    
     elif float(modify) != float(mtime):
-        return False    # File modify time is incorrect
+        return False    
     elif float(access) != float(atime):
-        return False    # File access time is incorrect
+        return False    
     return True
+    # Tests the decoy files against the legitimate values. If any changes have been made, the checkDecoyFiles function sends an alert.
 
 def checkDecoyFiles():
     with open("decoys.txt","r") as f:
+    # decoys.txt contains timestamps for two decoy files.
         for line in f:
             vals = line.rstrip().split(",")
             if not checkTimestamps(vals[0],vals[1],vals[2],vals[3]):
-                print("%s has been tampered with." % vals[0])
+            # Because the file contains the legitimate timestamps for the decoys, 
+            # any tampering will come across as a change in one of the time values in the monitoring doc, decoys.txt.
+                print("%s has been tampered with." % vals[0]
 
 checkDecoyFiles()
