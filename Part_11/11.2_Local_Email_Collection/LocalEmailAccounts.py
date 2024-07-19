bb13af756d24d569d2dc0bbc1a637da540025e4d
Ian Pokres <80968123+ianp89@users.noreply.github.com>
2024-07-19 13:09:12 -0400

    Commented on LocalEmailAccounts.py to explain functionality.

--- Part_11/11.2_Local_Email_Collection/LocalEmailAccounts.py
+++ Part_11/11.2_Local_Email_Collection/LocalEmailAccounts.py
@@ -1,6 +1,11 @@
+# This script searches a local system for email storage and reveals full emails if stored locally.
+
 from libratom.lib.pff import PffArchive
+# The PffArchive function knows how to parse stored .pst files (Outlook email format).
+# Python's libratom library in general provides wide-ranging email analysis functionality.
 
 filename = "C:\\Users\\hepos\\Documents\\Outlook Files\\howard@howardposton.com.pst"
+# This would be the location of locally stored emails using Outlook client for specified local user and email address.  
 archive = PffArchive(filename)
 
 for folder in archive.folders():
@@ -9,3 +14,5 @@ for folder in archive.folders():
             print("Sender: %s" % message.get_sender_name())
             print("Subject: %s" % message.get_subject())
             print("Message: %s" % message.get_plain_text_body())
+# Iterates through Outlook storage location specified above, tests if it has messages stored, and then reveals any emails that are there.
+# Could use Regex afterwards to search for information there, for instance.
