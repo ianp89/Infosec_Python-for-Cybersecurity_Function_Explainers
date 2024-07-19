60205486f856e980e70dcdefd02a409ff35424d8
Ian Pokres <80968123+ianp89@users.noreply.github.com>
2024-07-19 13:48:19 -0400

    Commented on EncryptedChannelClient.py to explain functionality.

--- Part_12/12.1_Encrypted_Channel/EncryptedChannelClient.py
+++ Part_12/12.1_Encrypted_Channel/EncryptedChannelClient.py
@@ -3,14 +3,17 @@ from Crypto.Cipher import AES
 
 host = "127.0.0.1"
 port = 1337
-
 key = b"Sixteen byte key"
+# This theoretical encrypted channel functions solely on port 1337 of the local host, 
+# but this information would be modified for sender and recipient.
+# AES works with a pre-shared key, given here as "key."
 
 def encrypt(data,key,iv):
-    # Pad data as needed
     data += " "*(16 - len(data) % 16)
-
+    # AES only works for data in 16 byte blocks, so this line finds how many spaces necessary to complete 16 bytes.
     cipher = AES.new(key,AES.MODE_CBC,iv)
+    # CBC is a "mode of operation," needs to be identical on both sides of the connection;
+    #basically establishes how the encryption method deals with data longer than its maximum encryptable datablock size.
     return cipher.encrypt(bytes(data,"utf-8"))
 
 message = "Hello"
